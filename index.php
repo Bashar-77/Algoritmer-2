24be6476b9792158d262e237244baf027a7da2a9
Bashar-77 <bashar.khallouf@elev.ga.ntig.se>
2023-01-24 13:43:54 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Algoritmer-2</title>
+</head>
+<body>
+    <?php
+    function Algoritmer2($x) {
+        return $x + 5;
+    }
+    echo Algoritmer2(1000000)
+
+    ?>
+
+    
+</body>
+</html>
